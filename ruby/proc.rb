383e5880c630adc341c28a718d6a94e39f60fea5
KY-Kshitij <kshitijyadav010@gmail.com>
2026-07-29 11:03:58 +0530

    proc with next and return

---
+++ ruby/proc.rb
@@ -0,0 +1,63 @@
+#Proc created inside method, called outside method
+
+#This is a very important edge case.
+
+def create_proc
+  Proc.new do
+    puts "inside proc"
+  end
+end
+
+x = create_proc
+x.call
+
+#This causes a LocalJumpError.
+
+#while returning anything inside proc 
+# it will not only return from the proc but also from the method.
+
+
+#A lambda's return returns from the lambda itself, not the surrounding method.
+
+# next inside Proc
+
+# This is different:
+
+def t
+  x = Proc.new do
+    print "p2"
+    next
+    print "p3" #not executed
+  end
+
+  x.call
+
+  print "p4"
+end
+
+#output p2p4
+
+def t
+  x = Proc.new do
+    next 100
+  end
+
+  result = x.call
+
+  puts result
+end
+
+#Output: 100
+
+def t
+  x = Proc.new do
+    print "p2"
+    return
+  end
+
+  print "p1"
+  x.call
+  print "p3"
+end
+
+#output: p1p2
