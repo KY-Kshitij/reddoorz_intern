93fcecdaf429e9845e8c1f1c22382b3c230efad6
KY-Kshitij <kshitijyadav010@gmail.com>
2026-07-29 11:46:13 +0530

    monkeypatching in ruby and naming conventions

---
+++ ruby/monkeypatching.rb
@@ -0,0 +1,15 @@
+class String
+  def shout
+    upcase + "!"
+  end
+end
+
+puts "ruby".shout
+
+#Output
+
+#RUBY!
+
+#You just added a method to Ruby's built-in String.
+
+#This is called Monkey Patching.
